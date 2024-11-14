b72301ca380a5753041e46b7f34a72a176380e31
Fabien <fafa.verhulst@gmail.com>
2024-11-14 16:45:36 +0100

    ajout nom

--- client/src/components/Hero/PresRoom/NameRoom/NameRoom.tsx
+++ client/src/components/Hero/PresRoom/NameRoom/NameRoom.tsx
@@ -1,9 +1,26 @@
+import { useContext } from "react";
+import CopieApiContext from "../../../Context/CopieApi";
+import IndexRoomContext from "../../../Context/IndexRoom";
+
 const NameRoom = () => {
+    //récupéré index
+    const contextIndex = useContext(IndexRoomContext);
+    if (!contextIndex) {
+      throw new Error("IndexRoomContext non valide");
+    }
+    const { indexRoom } = contextIndex;
+      //récupéré copie API
+  const contextCopieApi = useContext(CopieApiContext);
+  if (!contextCopieApi) {
+    throw new Error("IndexRoomContext non valide");
+  }
+  const { copieApi } = contextCopieApi;
+
   return (
-    <>
-      <p>La chambre industrielle</p>
-    </>
+    <div className="room">
+      <p>{copieApi[indexRoom].nom } </p>
+    </div>
   );
-};
+}
 
 export default NameRoom;
