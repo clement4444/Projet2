0d4a3b28723c2daea09944807591d96b38e08762
clément4444 <clement.fiquet0@gmail.com>
2024-11-14 16:54:27 +0100

    texte des option dynamique

--- client/src/components/Hero/PresRoom/OptionRoom/IconOption/IconOption.tsx
+++ client/src/components/Hero/PresRoom/OptionRoom/IconOption/IconOption.tsx
@@ -1,9 +1,30 @@
-const IconOption = () => {
-  return (
-    <>
-      <p>🐦‍🔥service</p>
-    </>
-  );
+import { useContext } from "react";
+import CopieApiContext from "../../../../Context/CopieApi";
+import IndexRoomContext from "../../../../Context/IndexRoom";
+
+const IconOption = ({ numeros }: { numeros: number }) => {
+  //récupéré index
+  const contextIndex = useContext(IndexRoomContext);
+  if (!contextIndex) {
+    throw new Error("IndexRoomContext non valide");
+  }
+  const { indexRoom } = contextIndex;
+
+  //récupéré copie API
+  const contextCopieApi = useContext(CopieApiContext);
+  if (!contextCopieApi) {
+    throw new Error("IndexRoomContext non valide");
+  }
+  const { copieApi } = contextCopieApi;
+
+  //afficher le composant que si il a le nombre élément
+  if (copieApi[indexRoom].equipements.length >= numeros + 1) {
+    return (
+      <>
+        <p>🐦‍🔥{copieApi[indexRoom].equipements[numeros]}</p>
+      </>
+    );
+  }
 };
 
 export default IconOption;
--- client/src/components/Hero/PresRoom/OptionRoom/OptionRoom.tsx
+++ client/src/components/Hero/PresRoom/OptionRoom/OptionRoom.tsx
@@ -3,10 +3,10 @@ import IconOption from "./IconOption/IconOption";
 const OptionRoom = () => {
   return (
     <>
-      <IconOption />
-      <IconOption />
-      <IconOption />
-      <IconOption />
+      <IconOption numeros={0} />
+      <IconOption numeros={1} />
+      <IconOption numeros={2} />
+      <IconOption numeros={3} />
     </>
   );
 };
