28f0553ef00714fb6544a7f0cc24e65e4b1555c6
Fabien <fafa.verhulst@gmail.com>
2024-11-14 17:12:20 +0100

    modif chambre-personne

--- client/src/components/Hero/PresRoom/BedNb/BedNb.tsx
+++ client/src/components/Hero/PresRoom/BedNb/BedNb.tsx
@@ -1,8 +1,26 @@
+import { useContext } from "react";
+import CopieApiContext from "../../../Context/CopieApi";
+import IndexRoomContext from "../../../Context/IndexRoom";
+
 const BedNb = () => {
+  //récupéré index
+  const contextIndex = useContext(IndexRoomContext);
+  if (!contextIndex) {
+    throw new Error("IndexRoomContext non valide");
+  }
+  const { indexRoom } = contextIndex;
+
+  //récupéré copie API
+  const contextCopieApi = useContext(CopieApiContext);
+  if (!contextCopieApi) {
+    throw new Error("IndexRoomContext non valide");
+  }
+  const { copieApi } = contextCopieApi;
+
   return (
-    <>
-      <p>1 chambre • 2 personnes</p>
-    </>
+    <div className="room">
+     <p>1 chambre • {copieApi[indexRoom].capacite} personnes</p>
+    </div>
   );
 };
 
