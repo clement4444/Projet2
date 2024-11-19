907ae4e798952ff996312c5e721d8db29995f1fc
clément4444 <clement.fiquet0@gmail.com>
2024-11-19 09:18:40 +0100

    ajout not find des chambre + transition css

---
+++ client/src/components/Hero/PresRoom/NotFind/NotFind.tsx
@@ -0,0 +1,11 @@
+import "./NotFind.css";
+
+const NotFind = () => {
+  return (
+    <>
+      <p>aucun resultat trouver</p>
+    </>
+  );
+};
+
+export default NotFind;
--- client/src/components/Hero/PresRoom/PresRoom.tsx
+++ client/src/components/Hero/PresRoom/PresRoom.tsx
@@ -1,9 +1,11 @@
 import "./PresRoom.css";
 
+import { UseCopieApiContext } from "../../../hooks/UseCopieApi";
 import BedNb from "./BedNb/BedNb";
 import Descri from "./Descri/Descri";
 import LikeButton from "./LikeButton/LikeButton";
 import NameRoom from "./NameRoom/NameRoom";
+import NotFind from "./NotFind/NotFind";
 import Notation from "./Notation/Notation";
 import OptionRoom from "./OptionRoom/OptionRoom";
 import PictureRoom from "./PictureRoom/PictureRoom";
@@ -11,32 +13,39 @@ import Price from "./Price/Price";
 import ReservedButton from "./ReservedButton/ReservedButton";
 import Separator from "./Separator/Separator";
 
+//affciher les chambre
+const sectionPresRoom = (
+  <div className="presRoom">
+    <PictureRoom />
+    <div className="infoRoom">
+      <div className="description">
+        <BedNb />
+        <NameRoom />
+        <p className="RoomLieux">Strasbourg, France</p>
+        <OptionRoom />
+      </div>
+      <div className="roomBas">
+        <section>
+          <Descri />
+          <Notation />
+        </section>
+        <div className="tarif">
+          <Price />
+          <ReservedButton />
+        </div>
+      </div>
+      <LikeButton />
+    </div>
+  </div>
+);
+
 const PresRoom = () => {
+  //récupères copie api
+  const { copieApi } = UseCopieApiContext();
   return (
     <>
       <Separator />
-      <div className="presRoom">
-        <PictureRoom />
-        <div className="infoRoom">
-          <div className="description">
-            <BedNb />
-            <NameRoom />
-            <p className="RoomLieux">Strasbourg, France</p>
-            <OptionRoom />
-          </div>
-          <div className="roomBas">
-            <section>
-              <Descri />
-              <Notation />
-            </section>
-            <div className="tarif">
-              <Price />
-              <ReservedButton />
-            </div>
-          </div>
-          <LikeButton />
-        </div>
-      </div>
+      {copieApi.length > 0 ? sectionPresRoom : <NotFind />}
       <Separator />
     </>
   );
