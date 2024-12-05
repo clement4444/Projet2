95445977e8fac009449aab546aeb865baf520f55
clément4444 <clement.fiquet0@gmail.com>
2024-12-05 20:59:27 +0100

    ca marche ?

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    proxy: {
+      '/projet2': 'https://api-univers-1rmkxnbib-clement4444s-projects.vercel.app', // Remplacez cette URL par l'URL de votre API
+    },
   },
-  base: '/Projet2/',
 });
